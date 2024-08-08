07ff3bdf7b85e2e042bb54225557d745bb32fe04
pavsoldatov <snowschoe@gmail.com>
2024-08-08 13:09:24 +0300

    finish ui

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [IonicModule],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css'
+})
+export class FooterComponent {
+
+}
